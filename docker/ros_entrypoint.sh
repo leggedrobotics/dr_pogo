e304d6efa3b930270d62e5e1a825cdae2328a687
Cedric Keller <cedrkeller@ethz.ch>
2026-09-14 10:10:16 +0200

    Change base image

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# ROS 2 Jazzy lives in the radarsplat conda env of the base image
+# (rslethz/fognav:radarsplat-jetson), not under /opt/ros.
 set -e
-source /opt/ros/jazzy/setup.bash
+source /opt/conda/etc/profile.d/conda.sh
+conda activate radarsplat
 source /ws/install/setup.bash
 exec "$@"
